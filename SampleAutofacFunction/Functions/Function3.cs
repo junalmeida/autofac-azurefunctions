640bcee0a5f64e6d298ef4ab4367e52fbb3ae725
Marcos Junior <junalmeida@gmail.com>
2022-05-19 16:15:22 -0500

    Fix local telemetry client dependency (#26)
    * Fix local telemetry client dependency
    * Bump sample to NET 6
    * Add dotnet 6 build process

---
+++ SampleAutofacFunction/Functions/Function3.cs
@@ -0,0 +1,41 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SampleAutofacFunction.Services;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SampleAutofacFunction.Functions
+{
+    public class Function3 : IDisposable
+    {
+        private readonly IService1 _service1;
+        private readonly ILogger _logger;
+        private readonly Guid _id = Guid.NewGuid();
+
+        public Function3(IService1 service1, ILogger logger)
+        {
+            _service1 = service1;
+            _logger = logger;
+            _logger.LogWarning($"Creating {this}");
+        }
+
+        public void Dispose()
+        {
+            _logger.LogWarning($"Disposing {this}");
+        }
+
+        [FunctionName(nameof(Function3))]
+        public async Task Run([TimerTrigger("0 */5 * * * *", RunOnStartup = true)] TimerInfo timer)
+        {
+            await Task.Delay(2000);
+            Activity.Current.AddTag("Test", "Hello World");
+            _logger.LogInformation($"C# Timer trigger function processed.");
+        }
+
+        public override string ToString()
+        {
+            return $"{_id} ({nameof(Function1)})";
+        }
+    }
+}
